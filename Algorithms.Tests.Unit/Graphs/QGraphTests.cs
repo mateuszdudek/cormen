6b0200385f26a2dadb013f78d6262e7033467504
Mateusz <mateusz.dude@gmail.com>
2022-04-26 21:47:33 +0200

    Graph search

---
+++ Algorithms.Tests.Unit/Graphs/QGraphTests.cs
@@ -0,0 +1,132 @@
+﻿using Algorithms.Graphs;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Algorithms.Tests.Unit.Graphs
+{
+    public class QGraphTests
+    {
+        [Test]
+        public void Case1()
+        {
+            var solution = new QSolution();
+            var edges = new List<(string from, string to)>
+            {
+                ("AAA", "BBB"),
+                ("AAA", "BBB"),
+                ("BBB", "CCC"),
+                ("BBB", "AAA")
+            };
+
+            Assert.That(solution.HasConnection(edges, 1, "AAA", "BBB"), Is.EqualTo(true));
+        }
+
+        [Test]
+        public void Case2()
+        {
+            var solution = new QSolution();
+            var edges = new List<(string from, string to)>
+            {
+                ("AAA", "BBB"),
+                ("AAA", "BBB"),
+                ("BBB", "CCC"),
+                ("BBB", "AAA")
+            };
+
+            Assert.That(solution.HasConnection(edges, 1, "AAA", "CCC"), Is.EqualTo(false));
+        }
+
+        [Test]
+        public void Case3()
+        {
+            var solution = new QSolution();
+            var edges = new List<(string from, string to)>
+            {
+                ("AAA", "BBB"),
+                ("AAA", "BBB"),
+                ("BBB", "CCC"),
+                ("BBB", "AAA")
+            };
+
+            Assert.That(solution.HasConnection(edges, 2, "AAA", "CCC"), Is.EqualTo(true));
+        }
+
+        [Test]
+        public void Case4()
+        {
+            var solution = new QSolution();
+            var edges = new List<(string from, string to)>
+            {
+                ("AAA", "BBB"),
+                ("AAA", "BBB"),
+                ("BBB", "CCC"),
+                ("BBB", "AAA")
+            };
+
+            Assert.That(solution.HasConnection(edges, 4, "AAA", "CCC"), Is.EqualTo(true));
+        }
+
+        [Test]
+        public void Case5()
+        {
+            var solution = new QSolution();
+            var edges = new List<(string from, string to)>
+            {
+                ("AAA", "BBB"),
+                ("AAA", "BBB"),
+                ("BBB", "CCC"),
+                ("BBB", "AAA")
+            };
+
+            Assert.That(solution.HasConnection(edges, 3, "AAA", "CCC"), Is.EqualTo(false));
+        }
+
+        [Test]
+        public void Case6()
+        {
+            var solution = new QSolution();
+            var edges = new List<(string from, string to)>
+            {
+                ("AAA", "BBB"),
+                ("AAA", "BBB"),
+                ("BBB", "CCC"),
+                ("BBB", "AAA"),
+                ("CCC", "DDD")
+            };
+
+            Assert.That(solution.HasConnection(edges, 3, "AAA", "DDD"), Is.EqualTo(true));
+        }
+
+        [Test]
+        public void Case7()
+        {
+            var solution = new QSolution();
+            var edges = new List<(string from, string to)>
+            {
+                ("AAA", "BBB"),
+                ("AAA", "BBB"),
+                ("BBB", "CCC"),
+                ("BBB", "AAA"),
+                ("CCC", "DDD")
+            };
+
+            Assert.That(solution.HasConnection(edges, 5, "AAA", "DDD"), Is.EqualTo(true));
+        }
+
+        [Test]
+        public void Case8()
+        {
+            var solution = new QSolution();
+            var edges = new List<(string from, string to)>
+            {
+                ("AAA", "BBB"),
+                ("AAA", "BBB"),
+                ("BBB", "CCC"),
+                ("BBB", "AAA"),
+                ("CCC", "DDD")
+            };
+
+            Assert.That(solution.HasConnection(edges, 4, "AAA", "DDD"), Is.EqualTo(false));
+        }
+    }
+}
---
+++ Algorithms/Graphs/QGraph.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms.Graphs
+{
+    public class QGraph : Dictionary<string, Dictionary<string, int>>
+    {
+        public QGraph(IEnumerable<(string from, string to)> edges)
+        {
+            foreach(var edge in edges)
+            {
+                if (!this.ContainsKey(edge.from)) this.Add(edge.from, new Dictionary<string, int>());
+                if (!this[edge.from].ContainsKey(edge.to)) this[edge.from].Add(edge.to, 0);
+                this[edge.from][edge.to]++;
+            }
+        }
+    }
+
+    public class QSolution
+    {
+        public bool DFS(QGraph graph, int noTrips, string start, string end)
+        {
+            if (noTrips == 0) return start == end;
+            if (!graph.ContainsKey(start)) return false;
+            
+            foreach (var to in graph[start].Where(e => e.Value > 0).ToArray())
+            {
+                graph[start][to.Key]--;
+                if (DFS(graph, noTrips - 1, to.Key, end)) return true;
+                graph[start][to.Key]++;
+            }
+
+            return false;
+        }
+
+        public bool HasConnection(IEnumerable<(string from, string to)> edges, int noTrips, string start, string end)
+        {
+            var graph = new QGraph(edges);
+            return DFS(graph, noTrips, start, end);
+        }
+    }
+}
